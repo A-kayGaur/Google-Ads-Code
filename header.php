c3f594136536e2b6838e1ab55de99fdbc9139149
Akshay Kumar <51516385+A-kayGaur@users.noreply.github.com>
2022-02-08 16:40:15 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+if ( is_page( array( 'thank-you' ) ) ) { ?>
+
+<!-- Event snippet for Website lead conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-10838365507/u0kUCK6X5JsDEMOqkbAo'});
+</script>
+	
+<?php } else {} ?>	
